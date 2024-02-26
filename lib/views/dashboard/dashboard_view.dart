1f783d9e7cce39f14f3a2b4f930ab56cb509cdfe
Baptiste Salaud <baptiste.salaud@gmail.com>
2024-02-26 12:11:44 +0100

    initialize folder and main dart

---
+++ lib/views/dashboard/dashboard_view.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class DashboardView extends StatefulWidget {
+  const DashboardView({super.key});
+
+  @override
+  DashboardViewState createState() => DashboardViewState();
+}
+
+class DashboardViewState extends State<DashboardView> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have tapped the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
