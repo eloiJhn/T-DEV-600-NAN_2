d8e7cf18d6538c9378c82d619860f59f3a72fc66
Baptiste Salaud <baptiste.salaud@gmail.com>
2024-03-04 17:18:38 +0100

    feat: ⚡️get cards in a trello list and get a card when user click on it

--- lib/views/board/board_view.dart
+++ lib/views/board/board_view.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:trelltech/models/board.dart';
+import 'package:trelltech/models/trello_card.dart';
+import 'package:trelltech/models/trello_list.dart';
 import 'package:trelltech/repositories/api.dart';
-
-import '../../models/trello_list.dart';
+import 'package:trelltech/widgets/card_widget.dart';
 
 class BoardView extends StatefulWidget {
   final Board board;
@@ -18,11 +19,13 @@ class BoardView extends StatefulWidget {
 
 class BoardViewState extends State<BoardView> {
   String? accessToken;
+  late CarouselController carouselController;
 
   @override
   void initState() {
     super.initState();
     _initialize();
+    carouselController = CarouselController();
   }
 
   Future<void> _initialize() async {
@@ -35,11 +38,6 @@ class BoardViewState extends State<BoardView> {
     accessToken = prefs.getString('accessToken');
   }
 
-  Future<void> getAccessToken() async {
-    final prefs = await SharedPreferences.getInstance();
-    accessToken = prefs.getString('accessToken');
-  }
-
   Future<List<TrelloList>> _getLists() async {
     var apiKey = dotenv.env['TRELLO_API_KEY'];
     var lists = await getLists(apiKey!, accessToken!, widget.board.id);
@@ -48,32 +46,97 @@ class BoardViewState extends State<BoardView> {
     return listList;
   }
 
+  Future<List<TrelloCard>> _getCardsByList(String trelloListId) async {
+    var apiKey = dotenv.env['TRELLO_API_KEY'];
+    var cards = await getCards(apiKey!, accessToken!, trelloListId);
+    List<TrelloCard> listCard =
+        cards.map((item) => TrelloCard.fromJson(item)).toList();
+    return listCard;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(widget.board.name)),
-        body: Center(
-          child: FutureBuilder(
-              future: _getLists(),
-              builder: (context, AsyncSnapshot<List<TrelloList>> snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(child: CircularProgressIndicator());
-                } else {
-                  return CarouselSlider.builder(
-                    itemCount: snapshot.data?.length,
-                    itemBuilder: (BuildContext context, int item,
-                            int pageViewIndex) =>
-                        Text(snapshot.data![item].name),
-                    options: CarouselOptions(
+      appBar: AppBar(title: Text(widget.board.name)),
+      body: FutureBuilder(
+          future: _getLists(),
+          builder: (context, AsyncSnapshot<List<TrelloList>> snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              return CarouselSlider.builder(
+                  carouselController: carouselController,
+                  itemCount: snapshot.data?.length,
+                  options: CarouselOptions(
                       autoPlay: false,
-                      enlargeCenterPage: true,
-                      viewportFraction: 0.9,
-                      aspectRatio: 2.0,
-                      initialPage: 2,
-                    ),
-                  );
-                }
-              }),
-        ));
+                      scrollDirection: Axis.vertical,
+                      enlargeCenterPage: false,
+                      height: MediaQuery.of(context).size.height),
+                  itemBuilder:
+                      (BuildContext context, int item, int pageViewIndex) {
+                    ScrollController listScrollController = ScrollController();
+                    return Card(
+                        child: Column(children: [
+                      Text(
+                        snapshot.data![item].name.toUpperCase(),
+                        style: const TextStyle(fontSize: 30),
+                      ),
+                      Expanded(
+                          child: FutureBuilder(
+                              future: _getCardsByList(snapshot.data![item].id),
+                              builder: (context,
+                                  AsyncSnapshot<List<TrelloCard>> snapshot) {
+                                if (!snapshot.hasData) {
+                                  return const Center(
+                                      child: CircularProgressIndicator());
+                                } else {
+                                  return NotificationListener<
+                                          ScrollNotification>(
+                                      onNotification: (notification) {
+                                        if (notification
+                                                is ScrollEndNotification &&
+                                            listScrollController
+                                                    .position.extentAfter ==
+                                                0) {
+                                          carouselController.animateToPage(
+                                            item + 1,
+                                            duration: const Duration(
+                                                milliseconds: 500),
+                                            curve: Curves.easeInOut,
+                                          );
+                                        }
+                                        return false;
+                                      },
+                                      child: ListView.builder(
+                                          controller: listScrollController,
+                                          itemCount: snapshot.data?.length,
+                                          itemBuilder: (BuildContext context,
+                                              int index) {
+                                            return GestureDetector(
+                                              onTap: () => {
+                                                showDialog(
+                                                    context: context,
+                                                    builder: (BuildContext
+                                                            context) =>
+                                                        CardWidget(
+                                                            card: snapshot
+                                                                .data![index])),
+                                              },
+                                              child: Card(
+                                                color: Colors.grey[200],
+                                                child: ListTile(
+                                                  title: Text(snapshot
+                                                      .data![index].name),
+                                                ),
+                                              ),
+                                            );
+                                          }));
+                                }
+                              }))
+                    ]));
+                  });
+            }
+          }),
+    );
   }
 }
