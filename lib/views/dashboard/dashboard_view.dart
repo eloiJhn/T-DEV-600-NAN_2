32ee14f505325ef15d47706405d2fef9f62b729d
Baptiste Salaud <baptiste.salaud@gmail.com>
2024-03-04 14:50:49 +0100

    feat: ⚡️get Lists from a board and carousel it

--- lib/views/dashboard/dashboard_view.dart
+++ lib/views/dashboard/dashboard_view.dart
@@ -43,57 +43,6 @@ class DashboardViewState extends State<DashboardView> {
     return boardList;
   }
 
-  Widget buildUI(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(AppLocalizations.of(context)!.dashboard)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Votre token est:',
-            ),
-            Text(
-              accessToken ?? 'Aucun token stocké',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Flexible(
-              child: FutureBuilder(
-                  future: _getBoards(),
-                  builder: (context, AsyncSnapshot<List<Board>> snapshot) {
-                    if (!snapshot.hasData) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else {
-                      return ListView.builder(
-                          itemCount: snapshot.data?.length,
-                          scrollDirection: Axis.horizontal,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Row(children: [
-                              Text(snapshot.data![index].name),
-                              ElevatedButton(
-                                onPressed: () => Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => BoardView(
-                                            board: snapshot.data![index]))),
-                                child:
-                                    Text(AppLocalizations.of(context)!.openBoard),
-                              ),
-                            ]);
-                          });
-                    }
-                  }),
-            ),
-            ElevatedButton(
-              onPressed: () => app_disconnect(context),
-              child: Text(AppLocalizations.of(context)!.logout),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -102,7 +51,51 @@ class DashboardViewState extends State<DashboardView> {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
         } else {
-          return buildUI(context);
+          return Scaffold(
+            appBar:
+                AppBar(title: Text(AppLocalizations.of(context)!.dashboard)),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Flexible(
+                    child: FutureBuilder(
+                        future: _getBoards(),
+                        builder:
+                            (context, AsyncSnapshot<List<Board>> snapshot) {
+                          if (!snapshot.hasData) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          } else {
+                            return ListView.builder(
+                                itemCount: snapshot.data?.length,
+                                scrollDirection: Axis.horizontal,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Row(children: [
+                                    Text(snapshot.data![index].name),
+                                    ElevatedButton(
+                                      onPressed: () => Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => BoardView(
+                                                  board:
+                                                      snapshot.data![index]))),
+                                      child: Text(AppLocalizations.of(context)!
+                                          .openBoard),
+                                    ),
+                                  ]);
+                                });
+                          }
+                        }),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => app_disconnect(context),
+                    child: Text(AppLocalizations.of(context)!.logout),
+                  ),
+                ],
+              ),
+            ),
+          );
         }
       },
     );
